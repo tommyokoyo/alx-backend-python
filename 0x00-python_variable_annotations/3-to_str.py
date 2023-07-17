e876efaab3c7b65bada237ce1b2aebbe1084ed3b
tommyokoyo <okoyotommy@gmail.com>
2023-07-17 21:10:48 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    to_str annotation tutorial
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        :param n:
+        :return: Two concatenated strings
+    """
+
+    return str(n)
