b18c94a7122330b1e2b8e06f7204263179628de2
tommyokoyo <okoyotommy@gmail.com>
2023-07-17 21:08:23 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Floor annotation tutorial
+"""
+
+
+def concat(n: float) -> int:
+    """
+        :param n:
+        :return: Two concatenated strings
+    """
+
+    return int(n)
