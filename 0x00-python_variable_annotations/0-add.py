5c6ff25f5220bb90af692f15959c5e20f4b542df
tommyokoyo <okoyotommy@gmail.com>
2023-07-17 21:01:25 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Type annotation tutorial
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        :param a:
+        :param b:
+        :return: sum of two floats
+    """
+
+    return a + b
