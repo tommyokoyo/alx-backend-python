da53668521421cb5396750da531fac6b10980a01
tommyokoyo <okoyotommy@gmail.com>
2023-07-17 21:15:55 +0300

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Variable definitions
+    a: int of value 1
+    pi: float of value 3.14
+    i_understand_annotations: boolean True
+    school: str value Holberton
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
