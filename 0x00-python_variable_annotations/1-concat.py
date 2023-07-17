f67597ed03e78074aae45576a56f5212c9fd3993
tommyokoyo <okoyotommy@gmail.com>
2023-07-17 21:04:29 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Type annotation tutorial
+"""
+
+
+def add(str1: str, str2: str) -> str:
+    """
+        :param str1:
+        :param str2:
+        :return: Two concatenated strings
+    """
+
+    return str1 + str2
